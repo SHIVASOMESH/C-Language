d3b16799265efc2373a86335047547d0a156eb1e
MR.Somesh <104584612+SHIVASOMESH@users.noreply.github.com>
2023-03-01 18:06:21 +0530

    Add files via upload

---
+++ Information.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Name:- Somesh Maheshwaram\n");
+	printf("Brith Date:- 27/12/1998\n");
+	printf("Age:- Iam 24 year old\n");
+	printf("Adress:- D.k Nagar, Maharan, gododra near, surat, gujarat\n");
+}
