cfedb15ff4f25b217e073eb1a95b632f9fa27a67
MR.Somesh <104584612+SHIVASOMESH@users.noreply.github.com>
2023-03-02 17:39:18 +0530

    Add files via upload

---
+++ even and odd num.c
@@ -0,0 +1,17 @@
+int main()
+{
+	int num;
+	printf("Enter an interger :");
+	scanf("%d, num");
+	
+	if(num % 2 == 0){
+		printf("%d is even", num);
+	}
+		
+	else{
+		printf("%d is odd", num);
+	}	
+		
+	getch();
+
+}
